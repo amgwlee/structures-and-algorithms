1ba8547c54d45a217f0dde2bba0b660b6cac37dd
Aaron <amgwlee93@gmail.com>
2020-09-08 18:30:15 -0400

    Starting on Binary Tree

--- src/main/java/com/example/structures/BinaryTree.java
+++ src/main/java/com/example/structures/BinaryTree.java
@@ -2,4 +2,6 @@ package com.example.structures;
 
 public class BinaryTree {
 
+    Node root;
+
 }
---
+++ src/main/java/com/example/structures/Node.java
@@ -0,0 +1,15 @@
+package com.example.structures;
+
+public class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+
+}
